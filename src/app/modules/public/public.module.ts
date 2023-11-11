1c8d46b0d2bb9139629bfb84a83eb29c6d47148e
AndersonSJimenezSBS <anderson.jimenezi@sbseguros.co>
2023-11-10 20:58:39 -0500

    Ajuste de home

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { ContentPublicComponent } from './content-public/content-public.component';
 import { HeaderPublicComponent } from './header-public/header-public.component';
+import { FooterPublicComponent } from './footer-public/footer-public.component';
+import { SliderHomeComponent } from './slider-home/slider-home.component';
 
 
 @NgModule({
   declarations: [
     ContentPublicComponent,
-    HeaderPublicComponent
+    HeaderPublicComponent,
+    FooterPublicComponent,
+    SliderHomeComponent
   ],
   imports: [
     CommonModule,
