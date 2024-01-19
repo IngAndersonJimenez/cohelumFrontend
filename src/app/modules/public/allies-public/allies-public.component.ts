9ddaed03841ca0139349a1b87be7bd33c0f1a0a2
Johoxtum <Johoxtum@hotmail.com>
2024-01-19 00:49:53 -0500

    :recycle: Ajuste header dispositivo nosotros y formulario contactenós

--- src/app/modules/public/allies-public/allies-public.component.ts
+++ src/app/modules/public/allies-public/allies-public.component.ts
@@ -28,6 +28,23 @@ export class AlliesPublicComponent {
     this.listaAliados = this.allyService.getListOfAllies();
     this.departamentList = this.departamentService.getDepartamentAllActive();
     this.cityList = this.cityService.getCitiesAllActive();
+    this.responsiveOptions = [
+      {
+        breakpoint: '1400px',
+        numVisible: 3,
+        numScroll: 3
+      },
+      {
+        breakpoint: '1220px',
+        numVisible: 2,
+        numScroll: 2
+      },
+      {
+        breakpoint: '1100px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
   }
 
 
